e5f89a7ab50661e43eff43844b0588e0650d66c8
MichaelHumphrey7 <michael@mkmc.net>
2018-05-15 23:33:15 -0700

    Added libraries and changed code to run keypad only

--- sketch_apr19a/Keypad.cpp
+++ sketch_apr19a/Keypad.cpp
@@ -1,11 +1,19 @@
 #include "DataStructs.h"
 #include "Boolean.h"
+#include "Elegoo.h"
 
 void Keypad (void* data) {
   
-  /*KeypadData* keypadData = (KeypadData*) data;
+  KeypadData* keypadData = (KeypadData*) data;
   
-  for (int b=0; b<5; b++) {
+  for (int b=0; b<1; b++) {
+
+    TSPoint p = ts.getPoint();
+    if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+      p.x = tft.width() - map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+      p.y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0);
+    }
+    
     if (buttons[b].justReleased()) {
       // Serial.print("Released: "); Serial.println(b);
       buttons[b].drawButton();  // draw normal
@@ -13,7 +21,7 @@ void Keypad (void* data) {
     
     if (buttons[b].justPressed()) {
         buttons[b].drawButton(true);  // draw invert!
-        
+        /*
         // data menu button
         if (b == 0) {
           *keypadData->dataMenu = TRUE;
@@ -21,7 +29,7 @@ void Keypad (void* data) {
         }
         // toggle alarms button
         if (b == 1 && *keypadData->displayMenu) {
-          *keypadData->alarmAcknowledge = 1 - *keypadData->alarmAcknowledge;
+          *keypadData->alarmAcknowledge = (Bool)(1 - (int)*keypadData->alarmAcknowledge);
         } 
         if (b <= 2 && *keypadData->dataMenu) {
           *keypadData->displayMenu = TRUE;
@@ -30,9 +38,9 @@ void Keypad (void* data) {
           if (b == 3) *keypadData->measurementSelection = BLOOD;
           if (b == 4) *keypadData->measurementSelection = PULSE;
         }
-          
+        */
     }
-  }*/
+  }
 }
 
 
