b8b695847b64e3dee9ff79f28ed04b498c6696ac
John <john@D-10-19-128-114.dhcp4.washington.edu>
2018-06-02 18:16:51 -0700

    perfect

--- MEGA/RemoteDisp.cpp
+++ MEGA/RemoteDisp.cpp
@@ -4,6 +4,11 @@
 void RemoteDisp (void* data) {
   RemoteDispData* remoteDispData = (RemoteDispData*) data;
   char values[4];
+
+  if (systemTimeBase % 10 != 0)
+    return;
+  
+  Serial.println("\n\n\n\n");
   
   Serial.println("   Pocket Doctor (TM)");
   Serial.println("  Doctor's Name: Janet");
@@ -14,7 +19,7 @@ void RemoteDisp (void* data) {
   values[1] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 1];
   values[2] = '.';
   values[3] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 2];
-  Serial.print("Temperature: "); Serial.print(values); Serial.println("C");
+  Serial.print("Temperature: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" C");
   /*Systolic pressure: <systolic pressure> mm Hg
   Diastolic pressure: <diastolic pressure> mm Hg
   Pulse rate: <pulse rate> BPM
