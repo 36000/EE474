f946a4e8509bdda32945085aa40e141c8baff397
John <john@D-10-19-128-114.dhcp4.washington.edu>
2018-06-02 18:26:25 -0700

    usefule

--- MEGA/RemoteDisp.cpp
+++ MEGA/RemoteDisp.cpp
@@ -10,20 +10,27 @@ void RemoteDisp (void* data) {
   
   Serial.println("\n\n\n\n");
   
-  Serial.println("   Pocket Doctor (TM)");
-  Serial.println("  Doctor's Name: Janet");
-  Serial.println("  Patient's Name: John");
-  Serial.println("------------------------");
+  Serial.println("       Pocket Doctor (TM)");
+  Serial.println("      Doctor's Name: Janet");
+  Serial.println("      Patient's Name: John");
+  Serial.println("    ------------------------");
 
   values[0] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3];
   values[1] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 1];
   values[2] = '.';
   values[3] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 2];
-  Serial.print("Temperature: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" C");
-  /*Systolic pressure: <systolic pressure> mm Hg
-  Diastolic pressure: <diastolic pressure> mm Hg
-  Pulse rate: <pulse rate> BPM
-  EKG < Measured Frequency reading > Hz
-  Battery: <charge remaining> */
+  Serial.print("    Temperature: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" C");
+  for (int i = 0; i < 3; i++)
+    values[i] = (char) remoteDispData->bloodPressCorrectedBuf[bp1CorrId * 3 + i];
+  Serial.print("    Systolic pressure: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" mm Hg");
+  for (int i = 0; i < 3; i++)
+    values[i] = (char) remoteDispData->bloodPressCorrectedBuf[bp2CorrId * 3 + i + 8 * 3];
+  Serial.print("    Diastolic pressure: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" mm Hg");
+  for (int i = 0; i < 3; i++)
+    values[i] = (char) remoteDispData->pulseRateCorrectedBuf[prCorrId * 3 + i];
+  Serial.print("    Pulse Rate: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" BPM");
+  values[0] = values[1] = values[2] = values[3] = '0';
+  Serial.print("    EKG: "); for (int i = 0; i < 4; i++) Serial.print(values[i]); Serial.println(" Hz");
+  Serial.print("    Battery: "); if (*remoteDispData->batteryState < 100) Serial.print("0"); Serial.print(*remoteDispData->batteryState); Serial.println("/200");
 }
 
