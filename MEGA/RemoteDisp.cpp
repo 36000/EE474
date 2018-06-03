f3e46855e6bf8635638b84b45e2e78efe23d522a
John <john@D-10-19-128-114.dhcp4.washington.edu>
2018-06-02 18:10:41 -0700

    okonkwo died

--- MEGA/RemoteDisp.cpp
+++ MEGA/RemoteDisp.cpp
@@ -3,6 +3,22 @@
 
 void RemoteDisp (void* data) {
   RemoteDispData* remoteDispData = (RemoteDispData*) data;
+  char values[4];
+  
+  Serial.println("   Pocket Doctor (TM)");
+  Serial.println("  Doctor's Name: Janet");
+  Serial.println("  Patient's Name: John");
+  Serial.println("------------------------");
 
+  values[0] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3];
+  values[1] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 1];
+  values[2] = '.';
+  values[3] = (char) remoteDispData->tempCorrectedBuff[tCorrId * 3 + 2];
+  Serial.print("Temperature: "); Serial.print(values); Serial.println("C");
+  /*Systolic pressure: <systolic pressure> mm Hg
+  Diastolic pressure: <diastolic pressure> mm Hg
+  Pulse rate: <pulse rate> BPM
+  EKG < Measured Frequency reading > Hz
+  Battery: <charge remaining> */
 }
 
